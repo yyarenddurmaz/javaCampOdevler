1b017e7fb7953d4017cf9ca173f7f1a7cd3decd7
Yaren Durmaz <109617894+yyarenddurmaz@users.noreply.github.com>
2022-09-25 15:54:31 +0300

    Add files via upload

---
+++ ödev2/recapDemo_Classes/src/recapDemo_Classes/Main.java
@@ -0,0 +1,12 @@
+package recapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortislem=new DortIslem();
+		int sonuc= dortislem.Topla(6, 9);
+		System.out.println(sonuc);
+
+	}
+
+}
